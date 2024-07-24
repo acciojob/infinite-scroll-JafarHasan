40a6659dd12bf303e0d9e5ba691e428ffde496f6
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-07-24 13:24:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here!
+const list=document.getElementById('infi-list');
+function createList(cnt) {
+	for(let i=0;i<cnt;i++){
+		const li=document.createElement('li');
+		li.textContent=`Item ${list.children.length+1}`
+		list.appendChild(li)
+	}
+	
+}
+createList(10);
+list.addEventListener('scroll',function() {
+	if(this.scrollTop+this.clientHeight>=this.scrollHeight){
+		createList(2);
+	}
+})
 
